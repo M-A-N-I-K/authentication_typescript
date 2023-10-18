972853f6bf9145052ef55bec20a3ef9cb7017418
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-10-18 17:37:09 +0530

    Dockerfile and docker-compose.yaml added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import compression from "compression"
 import cors from "cors"
 import mongoose from "mongoose"
 import router from "./routes"
+
 require('dotenv').config();
 
 const app = express();
